dac7f5764d74648fa1f538af9f548ac85020eb18
Aman Kushwaha <amansinghkushwaha2003@gmail.com>
2025-07-06 23:06:09 +0530

    chore(swagger): adds swagger deps and module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector))); //for excluding password 
+
+  const config = new DocumentBuilder()
+    .setTitle('Comment Service API')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   
   await app.listen(process.env.PORT ?? 3000);
 }
