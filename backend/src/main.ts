29d0cfb04977757ed91ea101decd55b231d78180
Aman Kushwaha <amansinghkushwaha2003@gmail.com>
2025-07-10 16:00:00 +0530

    chore: cors fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,11 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'http://comment-service.vercel.app',
-      process.env.FRONTEND_URL || 'http://localhost:3000',
-    ],
+    origin: true,
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: [
